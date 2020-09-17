155414089bad06c3d2aa420bd88316c8217d5360
shakespete <petersyjuco@gmail.com>
2020-09-17 16:25:37 +1000

    Adds resolvers

--- fullstack-tutorial/start/server/src/schema.js
+++ fullstack-tutorial/start/server/src/schema.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server');
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   type Launch {
@@ -13,18 +13,18 @@ const typeDefs = gql`
     name: String
     type: String
   }
-  
+
   type User {
     id: ID!
     email: String!
     trips: [Launch]!
   }
-  
+
   type Mission {
     name: String
     missionPatch(size: PatchSize): String
   }
-  
+
   enum PatchSize {
     SMALL
     LARGE
@@ -37,9 +37,29 @@ const typeDefs = gql`
   }
 
   type Query {
-    launches: [Launch]!
+    launches(
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
     launch(id: ID!): Launch
     me: User
+  }
+
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection { # add this below the Query type as an additional type.
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
   }
 
   type Mutation {
@@ -47,7 +67,6 @@ const typeDefs = gql`
     cancelTrip(launchId: ID!): TripUpdateResponse!
     login(email: String): String # login token
   }
-  
 `;
 
 module.exports = typeDefs;
